49804a5f87c700f9b9721d3a448be0eb83747e14
Vikas <v33vikasgautam@gmail.com>
2023-04-12 03:23:37 +0530

    Cleaned up the project structure and fixed the task.json to accomodate that

--- src/mergesort.c
+++ src/mergesort.c
@@ -52,10 +52,10 @@ void merge(int* a, int low, int mid, int high) {
 void mergeSort(int* a, int low, int high) {
   if (low>=high) return;
   
-  int mid=(low+high)/2;
-  mergeSort(a,low,mid);
-  mergeSort(a,mid+1,high);
-  merge(a,low,mid,high);
+  int mid = (low+high)/2;
+  mergeSort(a, low, mid);
+  mergeSort(a, mid+1, high);
+  merge(a, low, mid, high);
 }
 
 int main() {
